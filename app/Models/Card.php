b50c4f23a0d3671658fafc774a7f42e055490f16
Nicky Semenza <nicky@nickysemenza.com>
2018-02-19 16:37:15 -0500

    imageURL attribute on cards model

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
+    protected $appends = ['imageURL'];
     public function attributes()
     {
         return $this->hasMany('App\Models\CardAttribute');
@@ -13,5 +14,9 @@ class Card extends Model
     public function user()
     {
         return $this->BelongsTo('App\Models\User');
+    }
+    public function getImageURLAttribute() {
+        //TODO: real url
+        return "http://via.placeholder.com/350?text=card+id".$this->id;
     }
 }
