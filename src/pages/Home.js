75720b21accb1b99e12c2e59188e0b6c1e8de955
Nicky Semenza <nicky@nickysemenza.com>
2018-02-19 17:46:54 -0500

    login button on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import '../styles/App.scss';
 import { Jumbotron } from 'reactstrap';
 import { APP_NAME } from '../config';
+import Web3Login from '../components/Web3Login';
 class HomePage extends Component {
   render() {
     return (
-      <Jumbotron>
-        <h1 className="display-3">{APP_NAME}</h1>
-      </Jumbotron>
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">{APP_NAME}</h1>
+        </Jumbotron>
+        <Web3Login />
+      </div>
     );
   }
 }
