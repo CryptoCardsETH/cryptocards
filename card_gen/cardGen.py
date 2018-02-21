af242c8085d8de5cc73d035ae7a4fae954b7f8da
bonetail <worleyd@purdue.edu>
2018-02-21 16:42:22 -0500

    initial commit

---
+++ card_gen/cardGen.py
@@ -0,0 +1,44 @@
+'''
+Classes:
+    Physical {
+        Tank
+        Weapons Master
+        Unarmed
+    }
+
+    Support {
+        Healer
+        Stat Buffer
+    }
+
+    Uber {
+        Mime
+        Evasive
+        Last Stand
+    }
+
+    Ranged {
+        Caster
+        Archer
+        Firearms
+    }
+
+Attributes {
+    Attack
+    Health
+    Speed
+    Aggression
+    WeakAgainst
+    StrongAgainst
+    PassiveAbility
+    Move1
+    Move2
+}
+
+
+
+Base Attribues Based on Classes:
+    Physical:
+
+
+'''
