1c454aaf3aaf8f2c33d83e58e4a44c18fdcbfff2
Nicky Semenza <nicky@nickysemenza.com>
2018-02-21 16:41:13 -0500

    update paths of protogen scripts

--- tools/proto/gen-proto.sh
+++ tools/proto/gen-proto.sh
@@ -5,13 +5,13 @@ echo "generating PHP via protoc..."
 protoc --php_out=../backend/proto \
 	   --grpc_out=../backend/proto \
 	   --plugin=protoc-gen-grpc=grpc/bins/opt/grpc_php_plugin \
-	   --proto_path=../go/src/GoRpc/rpcServer/ \
-	   ../go/src/GoRpc/rpcServer/rpcServer.proto;
+	   --proto_path=../ethereum_proxy/src/GoRpc/rpcServer/ \
+	   ../ethereum_proxy/src/GoRpc/rpcServer/rpcServer.proto;
 
 # protoc --php_out=out --grpc-php_out=composer_name=grpc/hello:out --plugin=protoc-gen-grpc-php=./vendor/bin/protoc-gen-grpc-php ./helloworld.proto
 
 echo "generating golang via protoc..."
 
-cd "$PWD/../go/src/GoRpc";
+cd "$PWD/../ethereum_proxy/src/GoRpc";
 protoc -I rpcServer/ rpcServer/rpcServer.proto \
 	   --go_out=plugins=grpc:rpcServer;
