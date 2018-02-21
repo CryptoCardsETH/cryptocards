3aa54706b6f8b637c1cc4cdc699f473182b08d6b
Nicky Semenza <nicky@nickysemenza.com>
2018-02-21 16:48:57 -0500

    fix gen proto

--- tools/proto/gen-proto.sh
+++ tools/proto/gen-proto.sh
@@ -2,16 +2,16 @@
 cd "$(dirname "$0")"
 echo "generating PHP via protoc..."
 # php:
-protoc --php_out=../backend/proto \
-	   --grpc_out=../backend/proto \
+protoc --php_out=../../backend/proto \
+	   --grpc_out=../../backend/proto \
 	   --plugin=protoc-gen-grpc=grpc/bins/opt/grpc_php_plugin \
-	   --proto_path=../ethereum_proxy/src/GoRpc/rpcServer/ \
+	   --proto_path=../../ethereum_proxy/src/GoRpc/rpcServer/ \
 	   ../ethereum_proxy/src/GoRpc/rpcServer/rpcServer.proto;
 
 # protoc --php_out=out --grpc-php_out=composer_name=grpc/hello:out --plugin=protoc-gen-grpc-php=./vendor/bin/protoc-gen-grpc-php ./helloworld.proto
 
 echo "generating golang via protoc..."
 
-cd "$PWD/../ethereum_proxy/src/GoRpc";
+cd "$PWD/../../ethereum_proxy/src/GoRpc";
 protoc -I rpcServer/ rpcServer/rpcServer.proto \
 	   --go_out=plugins=grpc:rpcServer;
