13c249928aff35586f29e252b42ee0956fb63315
Jason Shipp <shippj@purdue.edu>
2018-02-21 00:23:10 -0500

    added basic go rpc server and go test client for verification. protobuf template located at rpcServer/rpcServer.proto

---
+++ backend/go/src/GoRpc/greeter_client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	pb "GoRpc/rpcServer"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.GetBlank(context.Background(), &pb.BlankRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
---
+++ backend/go/src/GoRpc/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	pb "GoRpc/rpcServer"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) GetBlank(ctx context.Context, in *pb.BlankRequest) (*pb.BlankReply, error) {
+	return &pb.BlankReply{Message: "Hello" + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve %v", err)
+	}
+}
