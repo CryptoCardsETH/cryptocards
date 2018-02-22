18f3b74e6ee2d5f316f27863878ef08ed0be6f32
bonetail <worleyd@purdue.edu>
2018-02-21 23:25:17 -0500

    initial generation contract

--- eth_contracts/contracts/GenerateCard.sol
+++ eth_contracts/contracts/GenerateCard.sol
@@ -1,11 +1,19 @@
 pragma solidity ^0.4.17;
 
 // Generation 0 Card Creation
-contract InitialGenerate {
-
-}
-
-// Generate New Card After Battle
 contract Generate {
+
+    // compiled attributes value for the card
+    uint256 attributes;
+
+    // number of cards to generate
+    uint64 numberOfCards = 800
+
+    function initialGeneration(numberOfCards) public returns (uint256) {
+        uint256 cardID;
+
+        for (cardID = 1; cardID <= attributes; cardID++) {
+            
+    }
 
 }
