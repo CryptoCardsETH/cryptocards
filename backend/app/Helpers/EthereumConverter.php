7034a09ffd77aea7bed81b1dca33c9304c7f5b08
Nicky Semenza <nicky@nickysemenza.com>
2018-02-22 17:15:10 -0500

    convert bigint -> float in backend instead of frontend

---
+++ backend/app/Helpers/EthereumConverter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers;
+
+
+class EthereumConverter
+{
+    public static function convertETHPriceToFloat($integerPrice)
+    {
+        return $integerPrice/ pow(10,13);
+    }
+    public static function convertETHPriceToInt($ethPriceFloat)
+    {
+        return  $ethPriceFloat *  pow(10,13);
+    }
+}
