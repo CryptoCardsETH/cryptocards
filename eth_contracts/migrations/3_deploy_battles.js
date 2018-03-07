f5b36203f7dcdad4dc01aa15252aeaedfb6a8828
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2018-03-06 22:08:58 -0500

    Fix card ownership

--- eth_contracts/migrations/3_deploy_battles.js
+++
@@ -1,7 +0,0 @@
-var Battles = artifacts.require("Battles");
-var BattleGroups = artifacts.require("BattleGroups");
-
-module.exports = function(deployer) {
-	deployer.deploy(Battles);
-	deployer.deploy(BattleGroups);
-};
