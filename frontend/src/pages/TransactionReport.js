e079ac088284449d78508ffd136668644223768c
Benjamin Bemis <bbemis@purdue.edu>
2018-03-27 22:36:52 -0400

    deleting some dead code

--- frontend/src/pages/TransactionReport.js
+++ frontend/src/pages/TransactionReport.js
@@ -9,49 +9,6 @@ class TransactionReportPage extends Component {
   componentDidMount() {
     this.props.fetchTransactionReport();
   }
-
-  /**
-   * Creates a new Array of data, but inserts missing days with a price of 0.
-   */
-  addMissingDays(data) {
-    let allDays = this.getAllDays(data);
-    let i = 0,
-      j = 0;
-    while (i < allDays.length) {
-      let tmpDay = new Date(data[j].day);
-      if (j < data.length && allDays[i].date.getTime() === tmpDay.getTime()) {
-        allDays[i].price_sum = data[j].price_sum;
-        j++;
-      } else {
-        allDays[i].price_sum = 0;
-      }
-      allDays[i].day = `${allDays[i].date.getFullYear()}-${this.pad(
-        allDays[i].date.getMonth() + 1
-      )}-${this.pad(allDays[i].date.getUTCDate())}`;
-      i++;
-    }
-    return allDays;
-  }
-
-  /**
-   * Gets an Array of every date in the range of the data.
-   */
-  getAllDays(data) {
-    if (!data) return [];
-    let earliestDate = new Date(data[0].day);
-    let latestDate = new Date(data[data.length - 1].day);
-    let range = latestDate - earliestDate;
-    let lengthOfDay = 1000 * 60 * 60 * 24;
-    let numDays = range / lengthOfDay;
-    return Array.from({ length: numDays + 1 }, (v, i) => {
-      return { date: new Date(earliestDate.valueOf() + lengthOfDay * i) };
-    });
-  }
-
-  pad(number) {
-    return ('0' + number).slice(-2);
-  }
-
   render() {
     let report = this.props.stats.transaction_report;
     let intervalLength = 1000 * 60 * 60 * 24; // one day in milliseconds
