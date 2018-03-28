517da5345ef849d5b332ed5135401c67b96228e5
Nicky Semenza <nicky@nickysemenza.com>
2018-03-28 11:25:08 -0400

    making contract playground more generic

--- frontend/src/actions/contracts.js
+++ frontend/src/actions/contracts.js
@@ -1,4 +1,6 @@
 import apiFetch from './index';
+import Eth from 'ethjs-query';
+import EthContract from 'ethjs-contract';
 
 export const RECEIVE_CONTRACT_ADDRESSES = 'RECEIVE_CONTRACT_ADDRESSES';
 export function fetchContractAddresses() {
@@ -16,3 +18,25 @@ function receiveContractAddresses(contracts) {
     contracts
   };
 }
+
+export function getContractInstance(ABI, contractAddress) {
+  const eth = new Eth(window.web3.currentProvider);
+  const contract = new EthContract(eth);
+
+  const miniToken = contract(ABI).at(contractAddress);
+  return miniToken;
+}
+
+export const waitForTxToBeMined = async txHash => {
+  const eth = new Eth(window.web3.currentProvider);
+
+  let txReceipt;
+  while (!txReceipt) {
+    try {
+      txReceipt = await eth.getTransactionReceipt(txHash);
+    } catch (err) {
+      return console.log(err);
+    }
+  }
+  console.log('yayyayayay', txReceipt);
+};
--- frontend/src/pages/ContractTest.js
+++ frontend/src/pages/ContractTest.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import '../styles/App.scss';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Eth from 'ethjs-query';
-import EthContract from 'ethjs-contract';
 import { BattleGroupABI } from './../contracts';
-
-class DebugPage extends Component {
+import {
+  getContractInstance,
+  waitForTxToBeMined
+} from './../actions/contracts';
+class ContractPlaygroundPage extends Component {
   test = (senderAddress, contractAddress) => {
-    const eth = new Eth(window.web3.currentProvider);
-    const contract = new EthContract(eth);
-
-    const miniToken = contract(BattleGroupABI).at(contractAddress);
-
+    //todo: web3 check
+    const miniToken = getContractInstance(BattleGroupABI, contractAddress);
     miniToken
       .createBattleGroup(senderAddress, [1, 2, 3, 4, 5], {
         from: senderAddress
@@ -20,22 +18,10 @@ class DebugPage extends Component {
       .then(txHash => {
         console.log('Transaction sent');
         console.dir(txHash);
-        this.waitForTxToBeMined(txHash, eth);
+        waitForTxToBeMined(txHash);
       })
       .catch(console.error);
   };
-
-  async waitForTxToBeMined(txHash, eth) {
-    let txReceipt;
-    while (!txReceipt) {
-      try {
-        txReceipt = await eth.getTransactionReceipt(txHash);
-      } catch (err) {
-        return console.log(err);
-      }
-    }
-    console.log('yayyayayay');
-  }
 
   render() {
     let { loaded, addresses } = this.props.contract;
@@ -62,4 +48,6 @@ function mapStateToProps(state) {
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({}, dispatch);
 };
-export default connect(mapStateToProps, mapDispatchToProps)(DebugPage);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  ContractPlaygroundPage
+);
