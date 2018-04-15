5cfbbc6af81de51ca43ce0d54dfe6b20412c5843
Nicky Semenza <nicky@nickysemenza.com>
2018-04-15 16:58:03 -0400

    Button to create a battle form a group (calls contract)

--- frontend/src/components/BattleGroup.js
+++ frontend/src/components/BattleGroup.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Card, { CARD_TYPE_COLLECTION } from './Card';
+import { Button } from 'reactstrap';
+import BattleCreator from './BattleCreator';
 const BattleGroup = ({ group }) => {
   let { user, group_cards } = group;
+  //todo: disabled for in progress battle and stuff
   return (
-    <div>
+    <div style={{ border: '1px solid #d8d8d8' }}>
       USER: {user.nickname || user.id}
       <br />
       Group ID: {group.id} <br />
+      <BattleCreator battleGroupId={group.id} />
       <div className="row">
         {group_cards.map(gc => (
           <Card key={gc.id} card={gc.card} type={CARD_TYPE_COLLECTION} />
--- frontend/src/contracts.js
+++ frontend/src/contracts.js
@@ -4,5 +4,10 @@ export const ABIs = {};
 
 export const BattleGroupABI =
   CompiledContracts['BattleGroups.sol:BattleGroups']['interface'];
+export const BattleABI = CompiledContracts['Battles.sol:Battles']['interface'];
+
 export const CONTRACT_NAME_BATTLEGROUPS = 'BattleGroups';
+export const CONTRACT_NAME_BATTLES = 'Battles';
+
 ABIs[CONTRACT_NAME_BATTLEGROUPS] = BattleGroupABI;
+ABIs[CONTRACT_NAME_BATTLES] = BattleABI;
