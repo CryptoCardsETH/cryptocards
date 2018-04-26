1bfb4e1406b750acd8c45fa32dd5430036b8615f
Nicky Semenza <nicky@nickysemenza.com>
2018-04-25 22:01:24 -0400

    golang hot reloading!

--- ethereum_proxy/docker_run.sh
+++ ethereum_proxy/docker_run.sh
@@ -2,7 +2,12 @@
 case $1 in
     run)
          #go get -v ./...
-         go run GoRpc/server/main.go GoRpc/server/helpers.go
+        go get -v github.com/fschl/CompileDaemon
+        cd $GOPATH/src/github.com/fschl/CompileDaemon \
+            && go install
+        #go run GoRpc/server/main.go GoRpc/server/helpers.go
+        cd /ethereum_proxy/src
+        ../bin/CompileDaemon -build="go build GoRpc/server" -recursive=true -command="go run GoRpc/server/main.go GoRpc/server/helpers.go" -color=true
         ;;
     *)
         exec "$@"
